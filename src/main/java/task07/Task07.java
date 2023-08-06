cec8e5cff52d2886ce1e56aa757699bca5bb9493
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-08-06 20:46:42 +1000

    Task07

--- src/main/java/task07/Task07.java
+++ src/main/java/task07/Task07.java
@@ -1,8 +1,32 @@
-package task07;
-
 /**
  *Выведите новый список, содержащий первый и последний элемент изначального списка.
  */
 
+package task07;
+
+import java.util.ArrayList;
+
 public class Task07 {
+    public static void main(String[] args) {
+        ArrayList<String> stringList = new ArrayList<>();
+        stringList.add("first");
+        stringList.add("second");
+        stringList.add("third");
+        stringList.add("last");
+
+        ArrayList<String> newStringList = new ArrayList<>();
+
+        newStringList.add(stringList.get(0));
+        newStringList.add(stringList.get(stringList.size() - 1));
+
+
+        ArrayList<String> newStringList2 = new ArrayList<>();
+
+        for (int i = 0; i > -2; i--) {
+            newStringList2.add(stringList.get((stringList.size() - 1) * Math.abs(i)));
+        }
+
+        System.out.println("newStringList: " + newStringList);
+        System.out.println("newStringList2: " + newStringList2);
+    }
 }
